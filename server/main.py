fa92909e8a9385e18db9dd60f4886b02dc7d7d78
sherry182474 <118046031+sherry182474@users.noreply.github.com>
2023-03-05 19:37:17 +0700

    commit

--- server/main.py
+++ server/main.py
@@ -8,23 +8,23 @@ app_desc = """<h2>Try this app by uploading any image with `predict/image`</h2>"
 app = FastAPI(title="Tensorflow FastAPI Start Pack", description=app_desc)
 
 
-@app.get("/", include_in_schema=False)
+@app.get("/", include_in_schema=False) 
 async def index():
     return RedirectResponse(url="/docs")
 
 
-@app.post("/predict/image")
-async def predict_api(file: UploadFile = File(...)):
-    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
-    print("File name: ", file.content_type)
-    if not extension:
-        return "Image must be jpg or png format!"
+@app.post("/predict/image") 
+async def predict_api(file: UploadFile = File(...)): 
+    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png") 
+    print("File name: ", file.content_type) 
+    if not extension: 
+        return "Image must be jpg or png format!" 
 
-    image = read_image_file(await file.read())
-    prediction = predict(image)
+    image = read_image_file(await file.read()) 
+    prediction = predict(image) 
 
-    return prediction
+    return prediction 
 
 
 if __name__ == "__main__":
-    uvicorn.run(app,host="0.0.0.0")
+    uvicorn.run(app,host="localhost",port=1234)
